466babf67f348f2d8048b8cc66d5a8deb439b943
Prince Prajapat <harince140222@gmail.com>
2026-07-05 12:04:57 +0530

    Day 2 - Added Socket.IO client connection and username handling

--- public/script.js
+++ public/script.js
@@ -1,3 +1,22 @@
 const socket = io();
 
 console.log("Connected to NexChat");
+
+const usernameInput = document.getElementById("username");
+
+const errorMessage = document.getElementById("error-message");
+
+const joinButton = document.getElementById("join-btn");
+
+// Join Button Click Event
+joinButton.addEventListener("click", function (event) {
+
+    // Stop page refresh
+    event.preventDefault();
+
+    // Get input value
+    const username = usernameInput.value.trim();
+
+    console.log(username);
+
+});
